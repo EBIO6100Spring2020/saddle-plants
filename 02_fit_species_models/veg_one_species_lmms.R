1a519852f0176fc69113bf990e5e0a37c6dee595
swnordstrom <swnordstrom@email.wm.edu>
2020-02-22 15:39:36 -0700

    init a script in species modeling dir for linear models

---
+++ 02_fit_species_models/veg_one_species_lmms.R
@@ -0,0 +1,9 @@
+# Script for setting up models looking at veg abundance
+# Initially starting with all three sp. in the same script.
+# february 2020 - 
+# scott
+
+# Load packages
+library(ggplot2)
+library(dplyr)
+library(tidyr)
