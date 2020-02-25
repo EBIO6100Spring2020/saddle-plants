3e08b4870039b2bdc4d895bfd277ad744b9a90e6
swnordstrom <swnordstrom@email.wm.edu>
2020-02-24 19:02:46 -0700

    start LMMs script

--- 02_fit_species_models/veg_one_species_lmms.R
+++ 02_fit_species_models/veg_one_species_lmms.R
@@ -7,3 +7,32 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(lme4)
+
+veg.focs = read.csv('01_process_data/output/veg_focals_top_sans96.csv')
+
+head(veg.focs)
+
+with(veg.focs, table(USDA_code, year))
+
+# Now, set aside year 2015 - 2017 as testing data, 2018 as mega-test
+# This means 1989 - 2014 are training data
+
+veg.train = veg.focs %>% filter(year %in% 1989:2014)
+veg.valid = veg.focs %>% filter(year %in% 2015:2017)
+
+# As a first pass... pick Deschampsia (just because)
+
+dece.train = veg.train %>% filter(USDA_code %in% 'DECE')
+dece.valid = veg.valid %>% filter(USDA_code %in% 'DECE')
+
+nrow(dece.train)
+nrow(dece.valid)
+
+# Wait! Shoot. If we want binomial or presence/absence,
+# we need to have zeros.
+# Should export sampling design from OG dataset.
+
+# Feels bad to say that a plant is "missing" though if
+# it isn't the top hit though no?
+
