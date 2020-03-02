5f7f4c4b1676b43c580700b2612b10c94aa852c2
swnordstrom <swnordstrom@email.wm.edu>
2020-03-02 15:45:05 -0700

    fix predictions in RMSE, start GEROT data (but note prediction is still wrong)

--- 02_fit_species_models/veg_one_species_lmms.R
+++ 02_fit_species_models/veg_one_species_lmms.R
@@ -16,7 +16,11 @@ rmse = function(x, xpred) sqrt(sum((x - xpred)^2) / length(x))
 
 # Now, loading in data.
 
+# Vegetation data
 veg.focs = read.csv('01_process_data/output/veg_focals_top_sans96.csv')
+
+# Spatial data (going to take out slope and aspect for now)
+spatial.data = read.csv('00_raw_data/spatial_physical_data/Point_attributes.csv')
 
 head(veg.focs)
 
@@ -63,7 +67,11 @@ dece.pts.present = dece.train %>%
   rbind(pts.per %>% filter(year %in% 1989:2014) %>% select(year, plot) %>% mutate(n.obs = 0)) %>%
   group_by(year, plot) %>%
   summarise(n.obs = sum(n.obs)) %>%
-  ungroup()
+  ungroup() %>%
+  merge(y = spatial.data %>% select(SDL_GRDP, Slope, Aspect),
+        by.x = 'plot', by.y = 'SDL_GRDP') %>%
+  filter(!is.na(Aspect))
+
 
 head(dece.pts.present)
 table(dece.pts.present$n.obs)
@@ -74,7 +82,10 @@ dece.pts.present.val = dece.valid %>%
   rbind(pts.per %>% filter(year %in% 2015:2017) %>% select(year, plot) %>% mutate(n.obs = 0)) %>%
   group_by(year, plot) %>%
   summarise(n.obs = sum(n.obs)) %>%
-  ungroup()
+  ungroup()  %>%
+  merge(y = spatial.data %>% select(SDL_GRDP, Slope, Aspect),
+                   by.x = 'plot', by.y = 'SDL_GRDP') %>%
+  filter(!is.na(Aspect))
 
 # A super null model
 # Just glm, no random effects
@@ -84,13 +95,16 @@ dece.null = glm(cbind(n.obs, 100 - n.obs) ~ 1,
 
 summary(dece.null)
 fitted.values(dece.null)[1]
-# This model predicts that there will be 
+# This model predicts that there will be Deschampsia present in 7 points per
+# quadrat.
 
 rmse.dece.null = rmse(x = dece.pts.present.val$n.obs, 
-                      xpred = predict(dece.null, newdata = dece.pts.present.val))
+                      xpred = 100 * predict(dece.null, 
+                                            newdata = dece.pts.present.val,
+                                            type = 'response'))
 
 rmse.dece.null
-# 11.078
+# 20.30057
 
 # A model featuring a random effect of site
 # THIS is a better null model from a statistical standpoint
@@ -108,9 +122,78 @@ fitted.values(dece.rf.null) %>%
 # got a nice spread there as expected
 
 rmse.dece.rf.null = rmse(x = dece.pts.present.val$n.obs,
-                         xpred = predict(dece.rf.null, newdata = dece.pts.present.val))
+                         xpred = 100 * predict(dece.rf.null, 
+                                               newdata = dece.pts.present.val,
+                                               type = 'response'))
 rmse.dece.rf.null
-# 12.41678
+# 9.68749
 
-# *spits out water*
-# WHAT
+# Huge reduction in RMSE.
+# Continue with this random effect.
+
+
+# A model with slope and plot-level random effect
+# NOTE: rank-deficient model somehow? IDK how this is happening.
+dece.slp = glm(cbind(n.obs, 100 - n.obs) ~ Slope + (1 | plot),
+               family = 'binomial',
+               data = dece.pts.present)
+summary(dece.slp)
+
+rmse.dece.slp = rmse(x = dece.pts.present.val$n.obs,
+                     xpred = 100 * predict(dece.slp, 
+                                           newdata = dece.pts.present.val,
+                                           type = 'response'))
+rmse.dece.slp
+
+dece.rt.null = glmer(cbind(n.obs, 100 - n.obs) ~ (1 | year),
+                     family = 'binomial',
+                     data = dece.pts.present)
+summary(dece.rt.null)
+
+rmse.dece.rt.null = rmse(x = dece.pts.present.val$n.obs,
+                         xpred = 100 * predict(dece.rt.null, newdata = dece.pts.present.val,
+                                         type = 'response',
+                                         re.form = NA))
+rmse.dece.rt.null
+
+# Duh
+
+##### Try with another species
+
+gero.train = veg.train %>% filter(USDA_code %in% 'GEROT')
+gero.valid = veg.valid %>% filter(USDA_code %in% 'GEROT')
+
+gero.pts.present = gero.train %>%
+  group_by(year, plot) %>%
+  summarise(n.obs = n()) %>%
+  rbind(pts.per %>% filter(year %in% 1989:2014) %>% select(year, plot) %>% mutate(n.obs = 0)) %>%
+  group_by(year, plot) %>%
+  summarise(n.obs = sum(n.obs)) %>%
+  ungroup() %>%
+  merge(y = spatial.data %>% select(SDL_GRDP, Slope, Aspect),
+        by.x = 'plot', by.y = 'SDL_GRDP') %>%
+  filter(!is.na(Aspect))
+
+gero.pts.present.val = gero.valid %>%
+  group_by(year, plot) %>%
+  summarise(n.obs = n()) %>%
+  rbind(pts.per %>% filter(year %in% 2015:2017) %>% select(year, plot) %>% mutate(n.obs = 0)) %>%
+  group_by(year, plot) %>%
+  summarise(n.obs = sum(n.obs)) %>%
+  ungroup()  %>%
+  merge(y = spatial.data %>% select(SDL_GRDP, Slope, Aspect),
+        by.x = 'plot', by.y = 'SDL_GRDP') %>%
+  filter(!is.na(Aspect))
+
+
+# Null GEROT model
+
+gero.null = glm(cbind(n.obs, 100 - n.obs) ~ 1,
+                family = 'binomial',
+                data = gero.pts.present)
+fitted.values(gero.null)[1]
+
+rmse.gero.null = rmse(x = gero.pts.present.val,
+                      xpred = predict(gero.null, gero.pts.present.val))
+rmse.gero.null
+# wtf That is huge
