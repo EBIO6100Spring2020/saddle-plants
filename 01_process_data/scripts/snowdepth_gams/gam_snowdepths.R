7578b1664e9f64078f3458c1297a217e0f5d5178
swnordstrom <swnordstrom@email.wm.edu>
2020-04-05 18:37:01 -0600

    init gam snowdepth folder

--- 01_process_data/scripts/snowdepth_gams/gam_snowdepths.R
+++ 01_process_data/scripts/snowdepth_gams/gam_snowdepths.R
@@ -35,7 +35,20 @@ sndp = snowdep %>%
          jd = jd + ifelse(jd < 0, 365 + as.numeric(year %% 4), 0)) %>%
   ungroup() %>%
   select(-c(i, date.origin)) %>%
-  filter(!is.nan(mean_depth) | !is.na(mean_depth))
+  filter(!is.nan(mean_depth) | !is.na(mean_depth)) %>%
+  mutate(point_ID = factor(point_ID),
+         seas = factor(seas))
+
+ggplot(sndp) +
+  geom_line(aes(x = jd, y = mean_depth, group = interaction(point_ID, seas)), alpha = 0.02)
+# some evidence here for a global smoother
+
+ggplot(sndp) +
+  geom_line(aes(x = jd, y = mean_depth, 
+                group = interaction(point_ID, seas),
+                colour = seas), 
+            alpha = 0.1) +
+  scale_color_gradient(low = 'red', high = 'blue')
 
 str(sndp)
 hist(sndp$jd)
@@ -53,14 +66,39 @@ sn.gam = gam(mean_depth ~ s(jd, k = 10, bs = 'cc') +
 sn.gam         
 gratia::draw(sn.gam)
 
+sndp %>%
+  filter(point_ID %in% c(48, 72)) %>%
+  mutate(pred = predict(sn.gam, .)) %>%
+  mutate(point_ID = factor(point_ID)) %>%
+  ggplot(aes(x = jd)) +
+  geom_line(aes(y = mean_depth, group = interaction(seas, point_ID), linetype = point_ID), colour = 'purple') +
+  geom_point(aes(y = mean_depth), colour = 'purple') +
+  geom_line(aes(y = pred, group = interaction(seas, point_ID), linetype = point_ID)) +
+  facet_wrap(~ seas)
+# Okay, here the smoother shows up. This is good.
+# That smoother shape looks about right.
+# Here plot is treated as a random effect.
+# You can see plot-level effects. Fuck yes!
+# The plot effect appears to just be an intercept shift.
+# Let's try a year random effect next.
 
 sn.gam.y = gam(mean_depth ~ s(jd, k = 10, bs = 'cc') +
                  s(point_ID, bs = 're') +
                  s(seas, bs = 're'),
                data = sndp, method = 'REML')
-
 sn.gam.y         
 gratia::draw(sn.gam.y)
+
+sndp %>%
+  filter(point_ID %in% c(48, 72)) %>%
+  mutate(pred = predict(sn.gam.y, .)) %>%
+  mutate(point_ID = factor(point_ID)) %>%
+  ggplot(aes(x = jd)) +
+  geom_line(aes(y = mean_depth, group = interaction(seas, point_ID), linetype = point_ID), colour = 'purple') +
+  geom_point(aes(y = mean_depth), colour = 'purple') +
+  geom_line(aes(y = pred, group = interaction(seas, point_ID), linetype = point_ID)) +
+  facet_wrap(~ seas)
+
 
 sn.gam.y20 = gam(mean_depth ~ s(jd, k = 20, bs = 'cc') +
                    s(point_ID, bs = 're') +
@@ -101,10 +139,11 @@ sndp.pp %>%
   geom_line(aes(y = pred, group = seas)) +
   facet_wrap(~ seas)
 
+# Here, the GS model (group-varying smoother) 
 sn.gam.ygs = gam(mean_depth ~ s(jd, k = 10, bs = 'cc', m = 2) +
-                   s(jd, point_ID, k = 10, bs = 'fs', m = 2) +
-                   s(point_ID, bs = 're') +
-                   s(seas, bs = 're'),
+                              s(jd, point_ID, k = 10, bs = 'fs', m = 2) +
+                              s(point_ID, bs = 're') +
+                              s(seas, bs = 're'),
                  data = sndp, method = 'REML')
 
 gratia::plot(sn.gam.ygs)
@@ -170,3 +209,47 @@ sndp.gss2 %>%
 
 # these also suck!
 # ughhgh
+
+sn.gam.s = gam(mean_depth ~ t2(jd, seas, point_ID,
+                               bs = c("cc", "re", "re"),
+                               k = c(10, 12, 12),
+                               m = 2,
+                               full = TRUE),
+               data = sndp, method = 'REML')
+
+sndp.s = sndp %>% mutate(pred = predict(object = sn.gam.s)) 
+
+sndp.ss = read.csv('snow_gam_g_syear_preds.csv')
+
+sndp.ss %>%
+  filter(point_ID %in% 72) %>%
+  ggplot(aes(x = jd)) +
+  geom_line(aes(y = mean_depth, group = seas), colour = 'purple') +
+  geom_point(aes(y = mean_depth), colour = 'purple') +
+  geom_line(aes(y = pred, group = seas)) +
+  facet_wrap(~ seas)
+
+sndp.ss %>%
+  filter(point_ID %in% c(48, 72)) %>%
+  mutate(point_ID = factor(point_ID)) %>%
+  ggplot(aes(x = jd)) +
+  geom_line(aes(y = mean_depth, group = interaction(seas, point_ID), linetype = point_ID), colour = 'purple') +
+  geom_point(aes(y = mean_depth), colour = 'purple') +
+  geom_line(aes(y = pred, group = interaction(seas, point_ID), linetype = point_ID)) +
+  facet_wrap(~ seas)
+
+sndp.ss %>%
+  filter(seas %in% 2005 & point_ID %in% 1:16) %>%
+  ggplot(aes(x = jd)) +
+  geom_line(aes(y = mean_depth, group = seas), colour = 'purple') +
+  geom_point(aes(y = mean_depth), colour = 'purple') +
+  geom_line(aes(y = pred, group = seas)) +
+  geom_point(aes(y = pred)) +
+  facet_wrap(~ point_ID)
+
+sn.gam.i = gam(mean_depth ~ te(jd, seas, point_ID,
+                               bs = c("cc", "re", "re"),
+                               k = c(10, 12, 12),
+                               m = 2,
+                               full = TRUE),
+               data = sndp, method = 'REML')
