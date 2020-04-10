4a30bd29ac6a8be440075f0eec5bbbc3436aa51f
swnordstrom <swnordstrom@email.wm.edu>
2020-04-10 07:55:36 -0600

    add script for concatenating gam_i fits

--- 01_process_data/scripts/snowdepth_gams/concatenate_csvs.R
+++ 01_process_data/scripts/snowdepth_gams/concatenate_csvs.R
@@ -17,18 +17,18 @@ rm(list = ls())
 
 ### Concatenate input data.
 
-for (fin in read.table('data_inputs/split_input_filenames.txt')$V1) {
-  assign(paste0('data_', fin), 
-         read.csv(paste0('data_inputs/split_data/', fin)) %>%
-           mutate(mod = fin))
-}
+# for (fin in read.table('data_inputs/split_input_filenames.txt')$V1) {
+#   assign(paste0('data_', fin), 
+#          read.csv(paste0('data_inputs/split_data/', fin)) %>%
+#            mutate(mod = fin))
+# }
+# 
+# do.call(mget(grep('data_', ls(), value = TRUE)), what = rbind) %>%
+#   write.csv(file = 'data_inputs/snowdepth_whole.csv', row.names = FALSE, quote = FALSE)
+# 
+# rm(list = ls())
 
-do.call(mget(grep('data_', ls(), value = TRUE)), what = rbind) %>%
-  write.csv(file = 'data_inputs/snowdepth_whole.csv', row.names = FALSE, quote = FALSE)
-
-rm(list = ls())
-
-### Concatenate gam fits
+### Concatenate gam_s fits
 
 # for (fin in paste0('outputs/', grep('snowgam_s', dir('outputs'), value = TRUE))) {
 #   identifier = gsub('outputs\\/snowgam\\_s\\_|\\.csv', '', fin)
@@ -41,4 +41,15 @@ rm(list = ls())
 # do.call(mget(grep('fit', ls(), value = TRUE)), what = rbind) %>%
 #   write.csv(file = 'outputs/all_gam_s_fits.csv', row.names = FALSE, quote = FALSE)
 
-        
+### Concatenate gam_i fits
+
+for (fin in paste0('outputs/', grep('snowgam_i', dir('outputs'), value = TRUE))) {
+  identifier = gsub('outputs\\/snowgam\\_i\\_|\\.csv', '', fin)
+  assign(paste0('fit', identifier), read.csv(fin) %>%
+    mutate(mod = fin))
+}
+
+ls()
+
+do.call(mget(grep('fit', ls(), value = TRUE)), what = rbind) %>%
+  write.csv(file = 'outputs/all_gam_i_fits.csv', row.names = FALSE, quote = FALSE)
