e12503e038bab45cc4d7cfa777416a901803d862
swnordstrom <swnordstrom@email.wm.edu>
2020-04-11 16:04:02 -0600

    incorporate logger temp data incl. some plots into exploration script

--- 01_process_data/exploration/temperature_exploration.R
+++ 01_process_data/exploration/temperature_exploration.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
 weath = read.csv("~/Downloads/knb-lter-nwt.413.10/sdltdayv.ml.data.csv")
 
 nrow(weath)
@@ -72,8 +76,127 @@ weath.thaw %>%
   ggplot() +
   geom_histogram(aes(x = jd, fill = seas), binwidth = 1)
 
-weath.thaw %>%
-  filter(cuml.thaw %in% 5) %>%
-  distinct(seas, .keep_all = TRUE) %>%
-  select(seas, )
-  print(n = 30)
+#### 
+
+# Advice from Cliff:
+
+# Growing season starts (?) with three consec. days of min. temp above -3C
+
+weath %>%
+  select(-c(LTER_site, local_site, flag_airtemp_max, flag_airtemp_min)) %>%
+  filter(seas > 1981) %>%
+  arrange(seas, jd) %>%
+  group_by(seas) %>%
+  mutate(a3 = airtemp_min > -3,
+         c3 = cumsum(a3),
+         d1 = c(0, diff(c3)),
+         d2 = c(0, diff(d1)),
+         d3 = c(0, diff(d2)),
+         flag = d1 & d2 & d3) %>% View()
+
+# This is hard to do with a dplyr chain.
+
+# Growing degree days is number of days with minimum daily temperature above 5deg C
+
+wggdn = weath %>%
+  filter(seas > 1981) %>%
+  arrange(seas, jd) %>%
+  group_by(seas) %>%
+  mutate(gddn = cumsum(airtemp_min > 5)) %>%
+  ungroup()
+
+ggplot(wggdn) +
+  geom_line(aes(x = jd, y = gddn, group = seas))
+
+# There are NAs here. There's also another data file.
+
+
+##### Try to merge together varios datas.
+
+# loggr is new data
+
+loggr = read.csv('00_raw_data/temp_new_loggers/sdlcr23x-cr1000.daily.ml.data.csv')
+
+head(loggr)
+
+table(loggr$LTER_site)
+table(loggr$local_site)
+table(loggr$logger)
+
+loggr = loggr %>%
+  select(-c(LTER_site, local_site)) %>%
+  mutate(year = date %>% as.character() %>% strsplit(split = '\\-') %>% 
+           sapply(function(x) unlist(x)[1] %>% as.numeric()),
+         mnth = date %>% as.character() %>% strsplit(split = '\\-') %>% 
+           sapply(function(x) unlist(x)[2] %>% as.numeric()),
+         dymn = date %>% as.character() %>% strsplit(split = '\\-') %>% 
+           sapply(function(x) unlist(x)[3] %>% as.numeric()),
+         seas = year + as.numeric(mnth > 9),
+         jdseas = jday - (274 + as.numeric(!year %% 4)),
+         jdseas = jdseas + ifelse(jdseas < 0, 365, 0))
+
+table(loggr$jdseas)
+
+# Plot: relative humiditiy
+
+loggr %>%
+  group_by(seas) %>%
+  mutate(ann_mean_rhs = mean(rh_avg, na.rm = TRUE)) %>%
+  ggplot(aes(x = jdseas)) +
+  geom_segment(aes(x = 0, xend = 365,
+                   y = ann_mean_rhs,
+                   yend = ann_mean_rhs),
+               colour = 'purple') +
+  geom_line(aes(y = rh_avg), size = 0.2) +
+  geom_ribbon(aes(ymin = rh_min, ymax = rh_max),
+              alpha = 0.2) +
+  facet_wrap(~ seas)
+
+# Not much annual difference in relative humidity.
+
+# Look at barometric pressure
+
+loggr %>%
+  group_by(seas) %>%
+  mutate(ann_mean_bp = mean(bp_avg, na.rm = TRUE)) %>%
+  ggplot(aes(x = jdseas)) +
+  geom_segment(aes(x = 0, xend = 365,
+                   y = ann_mean_bp,
+                   yend = ann_mean_bp),
+               colour = 'purple') +
+  geom_line(aes(y = bp_avg), size = 0.2) +
+  geom_ribbon(aes(ymin = bp_min, ymax = bp_max),
+              alpha = 0.2) +
+  facet_wrap(~ seas)
+
+# Not much annual difference in BP.
+
+# Look at solar radiation
+
+loggr %>%
+  group_by(seas) %>%
+  mutate(ann_mean_rad = mean(solrad_tot, na.rm = TRUE)) %>%
+  ggplot(aes(x = jdseas)) +
+  geom_segment(aes(x = 0, xend = 365,
+                   y = ann_mean_rad,
+                   yend = ann_mean_rad),
+               colour = 'purple') +
+  geom_line(aes(y = solrad_tot), size = 0.2) +
+  facet_wrap(~ seas)
+
+# Detrend
+
+loggr %>%
+  group_by(jdseas) %>%
+  mutate(dtrnd_mean_rad = solrad_tot - mean(solrad_tot, na.rm = TRUE)) %>%
+  group_by(seas) %>%
+  mutate(ann_mean_rad = mean(dtrnd_mean_rad[jdseas %in% 200:350], na.rm = TRUE)) %>%
+  ggplot(aes(x = jdseas)) +
+  geom_line(aes(y = dtrnd_mean_rad, group = seas), size = 0.25) +
+  geom_segment(aes(x = 200, xend = 350, 
+                   y = ann_mean_rad,
+                   yend = ann_mean_rad),
+               colour = 'purple') +
+  facet_wrap(~ seas)
+
+# No real evidence for differences in solar radiation  
