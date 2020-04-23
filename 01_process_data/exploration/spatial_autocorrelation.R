8b06f0e7caaaa32c939c2059a945fb9a64b97997
Thomas Merchant <thme3808@colorado.edu>
2020-04-23 09:36:55 -0600

    more spatial auto stuff

--- 01_process_data/exploration/spatial_autocorrelation.R
+++ 01_process_data/exploration/spatial_autocorrelation.R
@@ -2,6 +2,7 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(spdep)
+library(lme4)
 
 veg<-read.csv('01_process_data/output/veg_all_predictors.csv')
 head(veg)
@@ -38,11 +39,6 @@ idw[!is.finite(idw)]<-NA
 #I believe this give the estimate fo each plot based on the weights for all other plots * the amount fo dece in those plots. (in 2010)
 A<-colSums(veg.2010$n.obs*idw, na.rm = T)
 
-A.all.yrs<-veg%>%
-  filter(species %in% 'DECE')%>%
-  filter(!year %in% 1995)%>%#1995 does not appear to have all 88 plots 
-  group_by(year)%>%
-  mutate( A =colSums(n.obs*idw, na.rm = T) )
 
 #which year does not have 88 plots
 veg%>%
@@ -51,20 +47,122 @@ veg%>%
   group_by(year)%>%
   summarise(nplots = n())
 
-#very simple model
-summary(lm(data = A.all.yrs, n.obs~A))
 
+
+dece.A.all.years<-veg%>%
+  filter(species %in% 'DECE')%>%
+  filter(!year %in% 1995)%>%#1995 does not appear to have all 88 plots 
+  group_by(year)%>%
+  mutate( A =colSums(n.obs*idw, na.rm = T) )
+#very simple model
+summary(lm(data = dece.A.all.years, n.obs~A))
 
 ##ok to do this we first need to subset to consecutive years - 2010-2018
-prev.yr.sp.a<-A.all.yrs%>%
+dece.sp.a.yr<-dece.A.all.years%>%
   filter(year > 2008)%>%
   group_by(plot, year, A)%>%
   summarise(
     Ayr = year + 1
   )
-prev.yr.sp.a$year<-NULL
-colnames(prev.yr.sp.a)[2:3]<-c('A1', 'year')
+dece.sp.a.yr$year<-NULL
+colnames(dece.sp.a.yr)[2:3]<-c('A1', 'year')
   
-consec.yrs<-A.all.yrs%>%
+dece.consec.yrs<-dece.A.all.years%>%
   filter(year > 2008)
-A.consec.yrs<-merge(prev.yr.sp.a, consec.yrs, by = c('year', 'plot'))
+dece.A.consec.yrs<-merge(dece.sp.a.yr, dece.consec.yrs, by = c('year', 'plot'))
+
+#try mixed effects model
+dece.sp.a.null = glm(cbind(n.obs, 100 - n.obs) ~ 1, 
+                family = 'binomial',
+                data =A.consec.yrs)
+
+dece.sp.a.1 = glmer(cbind(n.obs, 100 - n.obs) ~ A1 +(1|year), 
+                     family = 'binomial',
+                     data =A.consec.yrs)
+summary(dece.sp.a.1)
+
+dece.sp.a.2 = glmer(cbind(n.obs, 100 - n.obs) ~ A1 +(1|year), 
+                    family = 'binomial',
+                    data =A.consec.yrs)
+summary(dece.sp.a.1)
+
+##geum
+gero.A.all.years<-veg%>%
+  filter(species %in% 'GEROT')%>%
+  filter(!year %in% 1995)%>%#1995 does not appear to have all 88 plots 
+  group_by(year)%>%
+  mutate( A =colSums(n.obs*idw, na.rm = T) )
+#very simple model
+summary(lm(data = gero.A.all.years, n.obs~A))
+
+##next we will use the previous years spatial autocorrelation to predict the next years abundance
+##ok to do this we first need to subset to consecutive years - 2010-2018
+gero.sp.a.yr<-gero.A.all.years%>%
+  filter(year > 2008)%>%
+  group_by(plot, year, A)%>%
+  summarise(
+    Ayr = year + 1
+  )
+gero.sp.a.yr$year<-NULL
+colnames(gero.sp.a.yr)[2:3]<-c('A1', 'year')
+
+gero.consec.yrs<-gero.A.all.years%>%
+  filter(year > 2008)
+gero.A.consec.yrs<-merge(gero.sp.a.yr, gero.consec.yrs, by = c('year', 'plot'))
+
+#try mixed effects model
+gero.sp.a.null = glm(cbind(n.obs, 100 - n.obs) ~ 1, 
+                     family = 'binomial',
+                     data =gero.A.consec.yrs)
+
+gero.sp.a.1 = glmer(cbind(n.obs, 100 - n.obs) ~ A1 +(1|year), 
+                    family = 'binomial',
+                    data =gero.A.consec.yrs)
+summary(gero.sp.a.1)
+
+gero.sp.a.2 = glmer(cbind(n.obs, 100 - n.obs) ~ A1 + (1|plot)+(1|year), 
+                    family = 'binomial',
+                    data =gero.A.consec.yrs)
+summary(gero.sp.a.2)
+
+##KOMY
+
+komy.A.all.years<-veg%>%
+  filter(species %in% 'KOMY')%>%
+  filter(!year %in% 1995)%>%#1995 does not appear to have all 88 plots 
+  group_by(year)%>%
+  mutate( A =colSums(n.obs*idw, na.rm = T) )
+#very simple model
+summary(lm(data = komy.A.all.years, n.obs~A))
+
+##next we will use the previous years spatial autocorrelation to predict the next years abundance
+##ok to do this we first need to subset to consecutive years - 2010-2018
+komy.sp.a.yr<-komy.A.all.years%>%
+  filter(year > 2008)%>%
+  group_by(plot, year, A)%>%
+  summarise(
+    Ayr = year + 1
+  )
+komy.sp.a.yr$year<-NULL
+colnames(komy.sp.a.yr)[2:3]<-c('A1', 'year')
+
+komy.consec.yrs<-komy.A.all.years%>%
+  filter(year > 2008)
+komy.A.consec.yrs<-merge(komy.sp.a.yr, komy.consec.yrs, by = c('year', 'plot'))
+
+#try mixed effects model
+komy.sp.a.null = glm(cbind(n.obs, 100 - n.obs) ~ 1, 
+                     family = 'binomial',
+                     data =komy.A.consec.yrs)
+
+
+komy.sp.a.1 = glmer(cbind(n.obs, 100 - n.obs) ~ A1 +(1|year), 
+                    family = 'binomial',
+                    data =komy.A.consec.yrs)
+summary(komy.sp.a.1)#not significant
+
+komy.sp.a.2 = glmer(cbind(n.obs, 100 - n.obs) ~ A1+(1|plot)+(1|year), 
+                    family = 'binomial',
+                    data =komy.A.consec.yrs)
+summary(komy.sp.a.2)#relationship minorly significant with plot level random effect
+
